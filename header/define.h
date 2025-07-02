978faecd13a1ca73f027da8ede24733c5c9e48f6
yanis <louanis@FAUXMAIL.com>
2025-07-02 15:08:44 +0200

    Modif commentaire define.h

--- header/define.h
+++ header/define.h
@@ -1,6 +1,6 @@
 #ifndef DEFINE_H
 #define DEFINE_H
 
-// On met ici toute les macros, les constantes de frames et les typedef pour les pointeurs de fonctions qu'on utilisera en attribut de nos ""classes""
+// On met ici toute les macros, les constantes de frames et toute les constantes en vrai jsp pk je separe les cas
 
 #endif // DEFINE_H
